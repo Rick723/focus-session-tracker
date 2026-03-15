37a9007916881992bc655b582c5433f4fa36224a
Rick723 <you@daichirikumiku0508@gmail.com>
2026-03-15 21:58:56 +0900

    トークン作成処理の追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,35 @@
 class ApplicationController < ActionController::Base
+  before_action :ensure_anonymous_user
+
+  private
+  #初回アクセス時にトークン作成メソッド（Cookie)
+    def ensure_anonymous_user
+      #Cookieの暗号化
+      token = cookies.encrypted[:anonymous_token]
+      user = nil
+
+      #トークンに合致したユーザーが存在しているかの判定
+      if token.present?
+        #ユーザー情報の取得
+        user = User.find_by(anonymous_token: token)
+      end
+
+      #ユーザーが存在していない場合新しいトークンを作成
+      if user.nil?
+        #ランダムに文字列を生成し、トークンにする
+        token = SecureRandom.hex(16)
+        #トークンがない場合新しくユーザーを登録
+        user = User.create!(anonymous_token: token)
+        #新しく作成したトークンと紐づけし、暗号化して保存
+        cookies.encrypted[:anonymous_token] = token
+      end
+
+        @current_user = user
+    end
+
+    #既に取得してあるユーザーを返すメソッド
+    def current_user
+      @current_user
+    end
+  
 end
