55aca0e35908c56667eb3960c8af6a02fa2c8f64
Rick723 <you@daichirikumiku0508@gmail.com>
2026-03-17 19:54:03 +0900

    ページ遷移のエラー修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,45 +2,40 @@ class ApplicationController < ActionController::Base
   before_action :ensure_anonymous_user
 
   private
-  #初回アクセス時にトークン作成メソッド（Cookie)
-    def ensure_anonymous_user
-      #Cookieの暗号化
-      token = cookies.encrypted[:anonymous_token]  = {
-                value: token,
-                expires: 3.months
-              }
-      user = nil
 
-      #トークンに合致したユーザーが存在しているかの判定
-      if token.present?
-        #ユーザー情報の取得
-        user = User.find_by(anonymous_token: token)
+  def ensure_anonymous_user
+    # まずはCookieからトークン文字列を読む
+    token = cookies.encrypted[:anonymous_token]
+    user = nil
 
-        # userを取得した際に有効期限を付与（毎回更新）
-        if user
-          cookies.encrypted[:anonymous_token] = {
-            value: token,
-            expires: 3.months
-          }
-        end
+    # トークンがあれば、対応するユーザーを探す
+    if token.present?
+      user = User.find_by(anonymous_token: token)
+
+      # ユーザーが見つかったときだけ、有効期限を更新して再保存
+      if user
+        cookies.encrypted[:anonymous_token] = {
+          value: token,
+          expires: 3.months
+        }
       end
-
-      #ユーザーが存在していない場合新しいトークンを作成
-      if user.nil?
-        #ランダムに文字列を生成し、トークンにする
-        token = SecureRandom.hex(16)
-        #トークンがない場合新しくユーザーを登録
-        user = User.create!(anonymous_token: token)
-        #新しく作成したトークンと紐づけし、暗号化して保存
-        cookies.encrypted[:anonymous_token] = token
-      end
-
-        @current_user = user
     end
 
-    #既に取得してあるユーザーを返すメソッド
-    def current_user
-      @current_user
+    # ユーザーが見つからなければ新規作成
+    if user.nil?
+      token = SecureRandom.hex(16)
+      user = User.create!(anonymous_token: token)
+
+      cookies.encrypted[:anonymous_token] = {
+        value: token,
+        expires: 3.months
+      }
     end
-  
+
+    @current_user = user
+  end
+
+  def current_user
+    @current_user
+  end
 end
