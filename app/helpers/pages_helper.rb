5a749e0811ac9a5cfa93e284465d74030f86d1e1
Rick723 <you@daichirikumiku0508@gmail.com>
2026-04-05 11:45:11 +0900

    カレンダー機能の詳細な実装

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,18 @@
 module PagesHelper
+  def focus_duration_label(total_seconds)
+    minutes = total_seconds.to_i / 60
+
+    "実測 #{minutes}分"
+  end
+
+  def pomo_doro_badge_class(kind)
+    case kind
+    when :completed
+      "calendar-badge calendar-badge-completed"
+    when :reached_five_minutes
+      "calendar-badge calendar-badge-five-minutes"
+    else
+      "calendar-badge calendar-badge-other"
+    end
+  end
 end
